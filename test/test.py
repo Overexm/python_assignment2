10cf3eb4aa5ca433ea850583c688cc24a6a4ff3c
Overexm <80208323+Overexm@users.noreply.github.com>
2021-10-10 17:15:24 +0600

    Add files via upload

---
+++ test/test.py
@@ -0,0 +1,16 @@
+from src import *
+
+some_data = getData()
+print("Please choose")
+print("1.With parameter")
+print("2.Without")
+check = input("Choose:")
+if check == '1':
+    curName = input("Enter currency name:")
+    scrapper = getDateByRequest(curName)
+    for i in scrapper:
+        print(i)
+elif check == '2':
+    for i in some_data:
+        print(i)
+
